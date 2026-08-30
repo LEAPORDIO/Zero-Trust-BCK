1df90162f5be074fba52690db848bff0e8e5dd5c
GOWRI SANKAR DOLA <gowrisankardola@GOWRIs-MacBook-Air.local>
2026-08-30 22:07:55 +0530

    Asdf

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5100/api",
+    baseURL: "https://zero-trust-bck.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
